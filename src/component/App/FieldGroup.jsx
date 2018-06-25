209edcc64ed6105d964727a95cde43681a62a67e
nabekou <nabekou0729@gmail.com>
2018-06-25 15:32:28 +0900

    Revert "fix: file name"
    This reverts commit 21940537a0b67c0a51164cb5e91167753ab326bb.

---
+++ src/component/App/FieldGroup.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+} from 'react-bootstrap';
+
+/**
+ * 入力フォームを生成します
+ * @param  {string}   id      ID
+ * @param  {string}   label   ラベル
+ * @param  {string}   help    ヘルプ
+ * @param  {Object}   props   その他の属性値
+ * @return {Object}           入力フォーム
+ */
+export function FieldGroup({
+  id,
+  label,
+  help,
+  ...props
+}) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl id={id} {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
